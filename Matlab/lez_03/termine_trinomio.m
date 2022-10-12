be8218286d0c392916c04c49935ceee1f5d8b0ab
Daniele <danieledaniele1988@gmail.com>
2022-10-12 18:11:29 +0200

    Aggiunta funzione bode_as.m
    Copiarla in una cartella PATH di MATLAB per richiamarla sempre,
    la versione caricata qui ha subito una leggera modifica rispetto a quella fornita
    dal docente, prende in input direttamente la funzione di trassferimento, come faceva anche
    la funzione "bode"

--- Matlab/lez_03/termine_trinomio.m
+++ Matlab/lez_03/termine_trinomio.m
@@ -6,11 +6,12 @@ zita_vet=linspace(0.01,1,10);
 figure()
 hold on;
 grid on;
+
 for i = 1:length(zita_vet)
     zita = zita_vet(i);
-    systf = tf([1/wn^2, 2*zita/wn, 1], 1);
-    bode(systf);
-    
+    systf = tf(1,[1/wn^2, 2*zita/wn, 1]);
+    bode_as(systf);
 end
+
 legend("zita = "+zita_vet);
 
