f08bd8e1d3f9b734e4a77f1bd25c901bdb26cb22
Daniele <danieledaniele1988@gmail.com>
2022-11-11 20:02:16 +0100

    Aggiunto margin

--- Matlab/lez_05/untitled2.m
+++ Matlab/lez_05/untitled2.m
@@ -1,10 +1,11 @@
 close all; clear;clc;
-syms s;
+syms s ;
 
-K = linspace(0.01,0.2,7);
+% K = linspace(0.01,0.2,2);
+K = 1*0.016;
 
-num = sym2poly(-0.5*(s-100));
-den = sym2poly(s^2+3.2*s+4);
+num = sym2poly(K*10*(s+1));
+den = sym2poly(s*(1+s*10)^2*(1+2*s));
 
 sys1 = tf(num,den);
 
@@ -17,14 +18,32 @@ nyquist(sys1);
 figure();
 bode_as(sys1);
 
+figure();
+nichols(sys1);
+
+figure();
+impulse(sys1)
+
+
+
+M = margin(sys1);
+T = feedback(sys1,1);
+
+figure();
+step(T);
+
+R = roots(T.Denominator{1});
 
 figure();
 hold on;
 grid on;
+
+out = sim('instabile');
+plot(out.y);
 
 for i=1:length(K)
 out = sim('instabile');
 plot(out.y);
 end
 
-legend("K = "+K);
+% legend("K = "+K);
